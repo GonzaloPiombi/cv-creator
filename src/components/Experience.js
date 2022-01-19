d41e9b21fd7e692bf0b6d6e95dbf077e84181954
Gonzalo <gonzalopiombi@gmail.com>
2022-01-19 11:15:32 -0300

    Add state to Experience component and created ExperienceList component.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,22 +1,32 @@
 import { Component } from 'react';
+import ExperienceList from './ExperienceList';
 
 class Experience extends Component {
-  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
+
+    this.state = {
+      addExperience: false,
+      experience: {
+        position: 'Position',
+        date: 'Date-Date',
+        company: 'Company name',
+        description: 'Description',
+      },
+      experiences: [],
+    };
   }
 
   render() {
-    return (
-      <div className="experience">
-        <h2>Position</h2>
-        <p>Date-Date</p>
-        <h3>Company Name</h3>
-        <p>Description</p>
-        <button>Add</button>
-        <button>Edit</button>
-      </div>
-    );
+    const { experience, experiences } = this.state;
+    if (!this.state.addExperience) {
+      return (
+        <div>
+          <ExperienceList />
+          <button>Add</button>
+        </div>
+      );
+    }
   }
 }
 
---
+++ src/components/ExperienceList.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class ExperienceList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ExperienceList;
