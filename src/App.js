348ad4f3507093399637f720af952a75b4a0ea45
Gonzalo <gonzalopiombi@gmail.com>
2022-01-24 14:39:48 -0300

    Refactor App into a functional component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import GeneralInformation from './components/GeneralInformation';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <GeneralInformation />
-        <Experience />
-        <Education />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <GeneralInformation />
+      <Experience />
+      <Education />
+    </div>
+  );
+};
 
 export default App;
