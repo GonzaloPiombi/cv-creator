338444a31d184c2f848ca0c5cbc260884ce58080
Gonzalo <gonzalopiombi@gmail.com>
2022-01-24 16:51:53 -0300

    Refactor code to use functional components.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,223 +1,205 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import ExperienceList from './ExperienceList';
 import uniqid from 'uniqid';
 
-class Experience extends Component {
-  constructor(props) {
-    super(props);
+const Experience = () => {
+  const [isAddingExperience, setIsAddingExperience] = useState(false);
+  const [experience, setExperience] = useState({
+    position: 'Position',
+    date: 'Date-Date',
+    company: 'Company name',
+    description: 'Description',
+    id: uniqid(),
+  });
+  const [experiences, setExperiences] = useState([]);
+  const [areFieldsBeingEdited, setAreFieldsBeingEdited] = useState(false);
 
-    this.state = {
-      addExperience: false,
-      experience: {
-        position: 'Position',
-        date: 'Date-Date',
-        company: 'Company name',
-        description: 'Description',
-        id: uniqid(),
-      },
-      experiences: [],
-      areFieldsBeingEdited: false,
-    };
-  }
-
-  addExperiences = () => {
-    this.setState({
-      addExperience: true,
-    });
+  const addExperience = () => {
+    setIsAddingExperience(true);
   };
 
-  handleSave = (e) => {
-    e.preventDefault();
-    this.setState({
-      experiences: this.state.experiences.concat(this.state.experience),
-      experience: {
-        position: 'Position',
-        date: 'Date-Date',
-        company: 'Company name',
-        description: 'Description',
-        id: uniqid(),
-      },
-      addExperience: false,
-      areFieldsBeingEdited: false,
+  const handleSave = (e) => {
+    setExperiences(experiences.concat(experience));
+    setExperience({
+      position: 'Position',
+      date: 'Date-Date',
+      company: 'Company name',
+      description: 'Description',
+      id: uniqid(),
     });
+    setIsAddingExperience(false);
+    setAreFieldsBeingEdited(false);
   };
 
-  handleEdit = (id, e) => {
-    e.preventDefault();
-    const newArr = [...this.state.experiences];
+  const handleEdit = (id, e) => {
+    const newArr = [...experiences];
     const index = newArr.findIndex((item) => item.id === id);
-    if (this.state.areFieldsBeingEdited) {
+    if (areFieldsBeingEdited) {
       newArr[index] = {
         ...newArr[index],
-        position: this.state.experience.position,
-        date: this.state.experience.date,
-        company: this.state.experience.company,
-        description: this.state.experience.description,
+        position: experience.position,
+        date: experience.date,
+        company: experience.company,
+        description: experience.description,
         id: uniqid(),
       };
     } else {
       newArr[index] = { ...newArr[index], id: uniqid() };
     }
-    this.setState({
-      experiences: newArr,
-      experience: {
-        position: 'Position',
-        date: 'Date-Date',
-        company: 'Company name',
-        description: 'Description',
-        id: uniqid(),
-      },
-      areFieldsBeingEdited: false,
+    setExperiences(newArr);
+    setExperience({
+      position: 'Position',
+      date: 'Date-Date',
+      company: 'Company name',
+      description: 'Description',
+      id: uniqid(),
     });
+    setAreFieldsBeingEdited(false);
   };
 
-  handleDelete = (id) => {
-    this.setState({
-      experiences: this.state.experiences.filter((item) => item.id !== id),
-    });
+  const handleDelete = (id) => {
+    const newExperiences = experiences.filter((item) => item.id !== id);
+    setExperiences(newExperiences);
   };
 
-  handlePositionChange = (e, item) => {
+  const handlePositionChange = (e, item) => {
     let newItem;
-    if (item && !this.state.areFieldsBeingEdited) {
+    if (item && !areFieldsBeingEdited) {
       newItem = { ...item, position: e.target.value, id: uniqid() };
     }
-    this.setState({
-      experience: newItem || {
+    setExperience(
+      newItem || {
         position: e.target.value,
-        date: this.state.experience.date,
-        company: this.state.experience.company,
-        description: this.state.experience.description,
-        id: this.state.experience.id,
-      },
-      areFieldsBeingEdited: true,
-    });
+        date: experience.date,
+        company: experience.company,
+        description: experience.description,
+        id: experience.id,
+      }
+    );
+    setAreFieldsBeingEdited(true);
   };
 
-  handleDateChange = (e, item) => {
+  const handleDateChange = (e, item) => {
     let newItem;
-    if (item && !this.state.areFieldsBeingEdited) {
+    if (item && !areFieldsBeingEdited) {
       newItem = { ...item, date: e.target.value, id: uniqid() };
     }
-    this.setState({
-      experience: newItem || {
-        position: this.state.experience.position,
+    setExperience(
+      newItem || {
+        position: experience.position,
         date: e.target.value,
-        company: this.state.experience.company,
-        description: this.state.experience.description,
-        id: this.state.experience.id,
-      },
-      areFieldsBeingEdited: true,
-    });
+        company: experience.company,
+        description: experience.description,
+        id: experience.id,
+      }
+    );
+    setAreFieldsBeingEdited(true);
   };
 
-  handleCompanyChange = (e, item) => {
+  const handleCompanyChange = (e, item) => {
     let newItem;
-    if (item && !this.state.areFieldsBeingEdited) {
+    if (item && !areFieldsBeingEdited) {
       newItem = { ...item, company: e.target.value, id: uniqid() };
     }
-    this.setState({
-      experience: newItem || {
-        position: this.state.experience.position,
-        date: this.state.experience.date,
+    setExperience(
+      newItem || {
+        position: experience.position,
+        date: experience.date,
         company: e.target.value,
-        description: this.state.experience.description,
-        id: this.state.experience.id,
-      },
-      areFieldsBeingEdited: true,
-    });
+        description: experience.description,
+        id: experience.id,
+      }
+    );
+    setAreFieldsBeingEdited(true);
   };
 
-  handleDescriptionChange = (e, item) => {
+  const handleDescriptionChange = (e, item) => {
     let newItem;
-    if (item && !this.state.areFieldsBeingEdited) {
+    if (item && !areFieldsBeingEdited) {
       newItem = { ...item, description: e.target.value, id: uniqid() };
     }
-    this.setState({
-      experience: newItem || {
-        position: this.state.experience.position,
-        date: this.state.experience.date,
-        company: this.state.experience.company,
+    setExperience(
+      newItem || {
+        position: experience.position,
+        date: experience.date,
+        company: experience.company,
         description: e.target.value,
-        id: this.state.experience.id,
-      },
-      areFieldsBeingEdited: true,
-    });
+        id: experience.id,
+      }
+    );
+    setAreFieldsBeingEdited(true);
   };
 
-  render() {
-    const { experience, experiences } = this.state;
-    if (!this.state.addExperience) {
-      return (
-        <section className="container">
-          <div className="section-title">
-            <h1>Experience</h1>
-            <span
-              className={['add-button', 'material-icons-outlined'].join(' ')}
-              onClick={this.addExperiences}
-            >
-              add_circle_outline
-            </span>
-          </div>
-          <ExperienceList
-            experiences={experiences}
-            onPositionChange={this.handlePositionChange}
-            onDateChange={this.handleDateChange}
-            onCompanyChange={this.handleCompanyChange}
-            onDescriptionChange={this.handleDescriptionChange}
-            onEdit={this.handleEdit}
-            onDelete={this.handleDelete}
-          />
-        </section>
-      );
-    }
+  if (!isAddingExperience) {
     return (
       <section className="container">
         <div className="section-title">
           <h1>Experience</h1>
+          <span
+            className={['add-button', 'material-icons-outlined'].join(' ')}
+            onClick={addExperience}
+          >
+            add_circle_outline
+          </span>
         </div>
-        <div className="column-section">
-          <ExperienceList
-            experiences={experiences}
-            onPositionChange={this.handlePositionChange}
-            onDateChange={this.handleDateChange}
-            onCompanyChange={this.handleCompanyChange}
-            onDescriptionChange={this.handleDescriptionChange}
-            onEdit={this.handleEdit}
-            onDelete={this.handleDelete}
-          />
-          <form className="column-section">
-            <input
-              type="text"
-              onChange={this.handlePositionChange}
-              defaultValue={experience.position}
-            ></input>
-            <input
-              type="text"
-              onChange={this.handleDateChange}
-              defaultValue={experience.date}
-            ></input>
-            <input
-              type="text"
-              onChange={this.handleCompanyChange}
-              defaultValue={experience.company}
-            ></input>
-            <input
-              type="text"
-              onChange={this.handleDescriptionChange}
-              defaultValue={experience.description}
-            ></input>
-            <span
-              className={['save-button', 'material-icons-outlined'].join(' ')}
-              onClick={this.handleSave}
-            >
-              save
-            </span>
-          </form>
-        </div>
+        <ExperienceList
+          experiences={experiences}
+          onPositionChange={handlePositionChange}
+          onDateChange={handleDateChange}
+          onCompanyChange={handleCompanyChange}
+          onDescriptionChange={handleDescriptionChange}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
       </section>
     );
   }
-}
+  return (
+    <section className="container">
+      <div className="section-title">
+        <h1>Experience</h1>
+      </div>
+      <div className="column-section">
+        <ExperienceList
+          experiences={experiences}
+          onPositionChange={handlePositionChange}
+          onDateChange={handleDateChange}
+          onCompanyChange={handleCompanyChange}
+          onDescriptionChange={handleDescriptionChange}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+        <form className="column-section">
+          <input
+            type="text"
+            onChange={handlePositionChange}
+            defaultValue={experience.position}
+          ></input>
+          <input
+            type="text"
+            onChange={handleDateChange}
+            defaultValue={experience.date}
+          ></input>
+          <input
+            type="text"
+            onChange={handleCompanyChange}
+            defaultValue={experience.company}
+          ></input>
+          <input
+            type="text"
+            onChange={handleDescriptionChange}
+            defaultValue={experience.description}
+          ></input>
+          <span
+            className={['save-button', 'material-icons-outlined'].join(' ')}
+            onClick={handleSave}
+          >
+            save
+          </span>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Experience;
--- src/components/ExperienceList.js
+++ src/components/ExperienceList.js
@@ -1,85 +1,74 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-class ExperienceList extends Component {
-  constructor(props) {
-    super(props);
+const ExperienceList = (props) => {
+  const [id, setId] = useState(null);
 
-    this.state = {
-      id: null,
-    };
-  }
-
-  handleEdit = (id) => {
-    this.setState({
-      id: id,
-    });
+  const handleEdit = (id) => {
+    setId(id);
   };
 
-  render() {
-    const { experiences } = this.props;
-    return experiences.map((item) => {
-      if (item.id === this.state.id) {
-        return (
-          <form key={item.id} className="column-section">
-            <input
-              type="text"
-              defaultValue={item.position}
-              placeholder={item.position}
-              onChange={(e) => this.props.onPositionChange(e, item)}
-            ></input>
-            <input
-              type="text"
-              defaultValue={item.date}
-              placeholder={item.date}
-              onChange={(e) => this.props.onDateChange(e, item)}
-            ></input>
-            <input
-              type="text"
-              defaultValue={item.company}
-              placeholder={item.company}
-              onChange={(e) => this.props.onCompanyChange(e, item)}
-            ></input>
-            <input
-              type="text"
-              defaultValue={item.description}
-              placeholder={item.description}
-              onChange={(e) => this.props.onDescriptionChange(e, item)}
-            ></input>
-            <span
-              className={['save-button', 'material-icons-outlined'].join(' ')}
-              onClick={this.props.onEdit.bind(this, item.id)}
-            >
-              save
-            </span>
-          </form>
-        );
-      }
+  return props.experiences.map((item) => {
+    if (item.id === id) {
       return (
-        <div key={item.id} className="ed-exp">
-          <div>
-            <h2>{item.position}</h2>
-            <p className="date">{item.date}</p>
-            <h3>{item.company}</h3>
-            <p>{item.description}</p>
-          </div>
-          <div>
-            <span
-              className={['edit-button', 'material-icons-outlined'].join(' ')}
-              onClick={this.handleEdit.bind(this, item.id)}
-            >
-              edit
-            </span>
-            <span
-              className={['delete-button', 'material-icons-outlined'].join(' ')}
-              onClick={this.props.onDelete.bind(this, item.id)}
-            >
-              delete
-            </span>
-          </div>
-        </div>
+        <form key={item.id} className="column-section">
+          <input
+            type="text"
+            defaultValue={item.position}
+            placeholder={item.position}
+            onChange={(e) => props.onPositionChange(e, item)}
+          ></input>
+          <input
+            type="text"
+            defaultValue={item.date}
+            placeholder={item.date}
+            onChange={(e) => props.onDateChange(e, item)}
+          ></input>
+          <input
+            type="text"
+            defaultValue={item.company}
+            placeholder={item.company}
+            onChange={(e) => props.onCompanyChange(e, item)}
+          ></input>
+          <input
+            type="text"
+            defaultValue={item.description}
+            placeholder={item.description}
+            onChange={(e) => props.onDescriptionChange(e, item)}
+          ></input>
+          <span
+            className={['save-button', 'material-icons-outlined'].join(' ')}
+            onClick={() => props.onEdit(item.id)}
+          >
+            save
+          </span>
+        </form>
       );
-    });
-  }
-}
+    }
+    return (
+      <div key={item.id} className="ed-exp">
+        <div>
+          <h2>{item.position}</h2>
+          <p className="date">{item.date}</p>
+          <h3>{item.company}</h3>
+          <p>{item.description}</p>
+        </div>
+        <div>
+          <span
+            className={['edit-button', 'material-icons-outlined'].join(' ')}
+            onClick={() => handleEdit(item.id)}
+          >
+            edit
+          </span>
+          <span
+            className={['delete-button', 'material-icons-outlined'].join(' ')}
+            onClick={() => props.onDelete(item.id)}
+          >
+            delete
+          </span>
+        </div>
+      </div>
+    );
+  });
+};
 
 export default ExperienceList;
